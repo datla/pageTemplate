b1aedc92ec0b58927b967dbc086fc74dbb7870d4
Suhael Akhtar <suhael.akhtar@maersk.com>
2015-10-19 16:44:45 +0100

    templates rendered server side

--- src/main/java/com/suhael/rest/PageResources.java
+++ src/main/java/com/suhael/rest/PageResources.java
@@ -2,31 +2,48 @@ package com.suhael.rest;
 
 import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Template;
-import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.ServletContextTemplateLoader;
 import com.github.jknack.handlebars.io.TemplateLoader;
 
+import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.io.IOException;
+
+import static javax.ws.rs.core.Response.ok;
 
 @Path("page")
 public class PageResources {
 
+    @Context
+    private ServletContext servletContext;
+
     @GET
     @Path("search")
     @Produces(MediaType.TEXT_HTML)
-    public String getHeader() throws IOException {
-        //return "<html><head></head><body>hello></body></html>";
+    public Response getHeader() throws IOException {
+        String content = "hello world";
+        return responseHTML(createHandlebarsTemplate().apply(content));
+    }
 
-        TemplateLoader loader = new ClassPathTemplateLoader();
-        loader.setPrefix("/resources/html");
-        loader.setSuffix(".html");
+    private Template createHandlebarsTemplate() throws IOException {
+        TemplateLoader loader = new ServletContextTemplateLoader(servletContext);
+        loader.setPrefix("/resources/template/core");
         Handlebars handlebars = new Handlebars(loader);
+        return handlebars.compile("core_template");
+    }
 
-        Template template = handlebars.compile("template");
+    private static Response responseHTML(Object entity) {
+        return response(entity, MediaType.TEXT_HTML);
+    }
 
-        return template.apply("Handlebars.java");
+    private static Response response(Object entity, String mediaType) {
+        return ok(entity)
+                .header("Content-Type", mediaType)
+                .build();
     }
 }
