f66045e247ae4150a565ba8607ea104718161b1f
Suhael Akhtar <suhael.akhtar@maersk.com>
2015-10-26 15:53:15 +0000

    get server data

--- src/main/webapp/resources/template/menu/menu.js
+++ src/main/webapp/resources/template/menu/menu.js
@@ -12,7 +12,7 @@ templates['menu'] = template({"1":function(container,depth0,helpers,partials,dat
     var stack1;
 
   return "<div class=\"center\">\n    <ul>\n"
-    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.menu : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.menuItems : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "    </ul>\n</div>";
 },"useData":true});
 })();
